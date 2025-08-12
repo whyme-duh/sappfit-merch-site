89c4223cbe47d67d8aca5ce512dcd0d37b381d9a
whyme <ritikshrestha94@gmail.com>
2025-08-12 21:36:06 +0545

    UI changes, making it responsive

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "192.168.1.76:5000"]
+ALLOWED_HOSTS = ["*", "192.168.1.76:8000"]
 
 
 # Application definition
