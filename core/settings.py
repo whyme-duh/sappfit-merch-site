8e9affbbeb2a50855860f2f22db2925b81391bfe
whyme <ritikshrestha94@gmail.com>
2026-01-30 19:38:31 +0545

    some mobile responsive issue fixed

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*",  " 192.168.101.13:8000"]
+ALLOWED_HOSTS = ["*",  "192.168.1.167:8000"]
 
 UNFOLD = {
     "SITE_TITLE": "SappFIt MerchSite",
