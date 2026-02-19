a5114307a31988611a9343407352655e81c07075
whyme <ritikshrestha94@gmail.com>
2026-02-19 21:58:22 +0545

    added a feature that shows the out of stock products in the admin dashboard

--- merchSite/dashboard.py
+++ merchSite/dashboard.py
@@ -1,7 +1,7 @@
 from django.db.models import Sum, Count
 from django.utils import timezone
 import datetime
-from merchSite.models import Order
+from merchSite.models import Order, Product
 
 
 def dashboard_callback(request, context):
@@ -9,13 +9,25 @@ def dashboard_callback(request, context):
     total_order = Order.objects.filter(status = "Delivered").count()
     pending_order = Order.objects.filter(status = "Pending").count()
     current_month = timezone.now().month
-
+    products = Product.objects.all()
+    out_of_stock_products = []
+    for product in products:
+        available_size = [size for size, value in product.size_options.items() if value > 0]
+        available_size_text = "Avilable" if available_size else "Out of Stock"
+        if available_size_text == "Out of Stock":
+            out_of_stock_products.append(product.name)
     current_month_name = datetime.datetime.now().strftime("%B")
     
     monthly_sales = Order.objects.filter(date__month = current_month, status = "Delivered").aggregate(Sum('price'))['price__sum'] or 0
 
     context.update({
         "kpi": [
+             {
+                "title": "Out Of Stock Products",
+                "metric": out_of_stock_products,
+                "color": "primary"
+            },
+            
             {
                 "title": "Total Revenue",
                 "metric": f"Rs. {total_income}",
